944070205d72745b502c2bb5a162a49a3f679d2f
emilePanchout <73824795+emilePanchout@users.noreply.github.com>
2025-11-14 15:21:22 +0100

    First commit/ test bomb

---
+++ Assets/Scripts/Bombs.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Bombs : MonoBehaviour
+{
+    public int difficulty;
+    public bool isDefused = false;
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int bombCount;
+    public GameObject bombSpawner;
+
+
+    public void StartGame()
+    {
+        bombCount = 1;
+    }
+
+    public void SpawnNextBomb()
+    {
+
+    }
+}
